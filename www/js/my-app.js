0f7f556282305e88da83f36a90996c1312b3a8dc
f7-template <125393410+f7-template@users.noreply.github.com>
2023-02-20 14:10:37 -0300

    f7 v.@7.1.2

---
+++ www/js/my-app.js
@@ -0,0 +1,74 @@
+//INICIALIZAÇÃO DO F7 QUANDO DISPOSITIVO ESTÁ PRONTO
+document.addEventListener('deviceready', onDeviceReady, false);
+var app = new Framework7({
+  // App root element
+  el: '#app',
+  // App Name
+  name: 'My App',
+  // App id
+  id: 'com.myapp.test',
+  // Enable swipe panel
+  panel: {
+    swipe: true,
+  },
+  dialog: {
+    buttonOk: 'Sim',
+    buttonCancel: 'Cancelar',
+  },
+  // Add default routes
+  routes: [
+    {
+      path: '/index/',
+      url: 'index.html',
+      animate: false,
+    },
+    {
+      path: '/link2/',
+      url: 'link2.html',
+      animate: false,
+    },
+    {
+      path: '/link3/',
+      url: 'link3.html',
+      animate: false,
+    },
+  ],
+  // ... other parameters
+});
+
+//Para testes direto no navegador
+//var mainView = app.views.create('.view-main', { url: '/index/' });
+
+//EVENTO PARA SABER O ITEM DO MENU ATUAL
+app.on('routeChange', function (route) {
+  var currentRoute = route.url;
+  console.log(currentRoute);
+  document.querySelectorAll('.tab-link').forEach(function (el) {
+    el.classList.remove('active');
+  });
+  var targetEl = document.querySelector('.tab-link[href="' + currentRoute + '"]');
+  if (targetEl) {
+    targetEl.classList.add('active');
+  }
+});
+
+
+function onDeviceReady() {
+  //Quando estiver rodando no celular
+  var mainView = app.views.create('.view-main', { url: '/index/' });
+
+  //COMANDO PARA "OUVIR" O BOTAO VOLTAR NATIVO DO ANDROID 	
+  document.addEventListener("backbutton", function (e) {
+
+    if (mainView.router.currentRoute.path === '/index/') {
+      e.preventDefault();
+      app.dialog.confirm('Deseja sair do aplicativo?', function () {
+        navigator.app.exitApp();
+      });
+    } else {
+      e.preventDefault();
+      mainView.router.back({ force: true });
+    }
+  }, false);
+
+}
